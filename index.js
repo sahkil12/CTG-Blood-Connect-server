e85e23e752e8e363163e1bbb4186804d0fd61220
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-16 23:00:49 +0600

    connect server site with mongodb atlas

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri = "mongodb+srv://CTG-Blood-Connect:5Z67moZwwxohiSNh@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri)
+
+async function run() {
+     try {
+          await client.connect();
+          // Send a ping to confirm a successful connection
+          await client.db("admin").command({ ping: 1 });
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     } finally {
+          // Ensures that the client will close when you finish/error
+          // await client.close();
+     }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
      res.send('CTG Blood Connect Server Running');
@@ -15,3 +33,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
 })
+
+// CTG-Blood-Connect
+// 5Z67moZwwxohiSNh
