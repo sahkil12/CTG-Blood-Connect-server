574ee2ba4ccc710ae90ad9aec5c86a1a85b91f78
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-17 22:18:32 +0600

    secure mongodb user and pass in .env file and also add post method to post donors data

--- index.js
+++ index.js
@@ -9,13 +9,36 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb connection
-const uri = "mongodb+srv://CTG-Blood-Connect:5Z67moZwwxohiSNh@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri)
+
+let donorsCollection;
 
 async function run() {
      try {
           await client.connect();
+          const db = client.db("CTG-Blood-Connect")
+          donorsCollection = db.collection("donors")
+
+          app.post('/donors', async (req, res) => {
+               try {
+                    const donor = req.body;
+                    
+                    donor.available = true;
+
+                    const result = await donorsCollection.insertOne(donor);
+
+                    res.status(201).json({
+                         message: 'Donor added successfully',
+                         insertedId: result.insertedId
+                    });
+               } catch (error) {
+                    res.status(500).json({ message: error.message });
+               }
+          });
+
+
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -33,6 +56,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
 })
-
-// CTG-Blood-Connect
-// 5Z67moZwwxohiSNh
