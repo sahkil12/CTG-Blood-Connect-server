25dba7039603f9c329c907b82043b9d28c183193
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-22 20:02:53 +0600

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new MongoClient(uri)
 
 async function run() {
      try {
-          await client.connect();
+          // await client.connect();
           const db = client.db("CTG-Blood-Connect")
           const donorsCollection = db.collection("donors")
           const usersCollection = db.collection("users")
