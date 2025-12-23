dfe89a51a3c18cb98b043cf51bf11faa313c6923
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-23 19:10:36 +0600

    add orgin in cors middleware

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const admin = require('firebase-admin');
 // middleware
-app.use(cors());
+app.use(
+     cors({
+          origin: [
+               "http://localhost:5173",
+               "https://ctg-blood-connect.web.app"
+          ],
+          credentials: true,
+     })
+);
 app.use(express.json());
 // firebase admin
 const serviceAccount = require('./firebase-admin-sdk.json');
